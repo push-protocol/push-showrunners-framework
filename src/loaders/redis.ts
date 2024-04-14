dfedfc9e15d4216ff67945245df18b0caa1fc60a
akanshaw-cloudside <141826364+akanshaw-cloudside@users.noreply.github.com>
2024-04-14 20:32:00 +0530

    Update redis.ts

--- src/loaders/redis.ts
+++ src/loaders/redis.ts
@@ -1,6 +1,9 @@
 const redis = require('async-redis');
 import config from '../config';
 
-const ReddisInstance = redis.createClient({ config.redisURL });
+const ReddisInstance = redis.createClient({ 
+        url: config.REDIS_URL,
+        password: config.REDIS_AUTH,   
+});
 
 export default ReddisInstance;
